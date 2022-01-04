19a0718b2ac8c878344a09b16b67580b4ddf5539
nova_sangeeth <novasangeeth@outlook.com>
2022-01-05 02:19:09 +0530

    Changes.

--- start_dev.sh
+++ start_dev.sh
@@ -1,4 +1,4 @@
 # start_dev
-echo"Starting to build and deploy the application."
+echo "Starting to build and deploy the application."
 docker-compose down && docker-compose build && docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d && docker-compose ps
-echo"Done..."
+echo "Done..."
